62136d48ad905105567fb5f5d73129f57aaeedef
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-06-05 15:32:18 +0700

    menampilkan/ display push value

--- Stack.cpp
+++ Stack.cpp
@@ -29,7 +29,9 @@ class Stack
         Node *newNode = new Node();
         newNode->data = value;
         newNode->next = top; 
-         top = newNode;   
+        top = newNode;   
+        cout << "Push Value" << value << endl;
+        return value;
     }
 };
 
