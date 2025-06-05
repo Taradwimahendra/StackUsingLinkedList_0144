33902ebbb1d8960912c6174237cc0243996948f6
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-06-05 16:01:14 +0700

    memasukkan data  ke fungsi utama

--- Stack.cpp
+++ Stack.cpp
@@ -68,5 +68,48 @@ class Stack
                  }
                  cout << endl;
             } 
+        }
 };
 
+int main()
+{
+    Stack stack;
+
+    int choice = 0;
+    int value;
+
+    while (choice != 4)
+    {
+       cout << "1. Push\n";
+       cout << "2. Pop\n";
+       cout << "3. Peek\n";
+       cout << "4. Exit\n";
+       cout << "Enter your choice: ";
+       cin >> choice;
+
+       switch (choice)
+       {
+       case 1:
+        cout << "Enter the value to push: ";
+        cin >> value;
+        stack.push(value); 
+       case 2:
+        stack.pop(); 
+        break;
+       case 3:
+        stack.peek(); 
+        break;
+       case 4:
+        cout << "Exiting Program." << endl;
+        break;
+       default:
+        cout << "Invalid choice. Try again." << endl;
+        break;
+       }
+
+       cout << endl;
+    }
+
+    return 0;
+    
+}
